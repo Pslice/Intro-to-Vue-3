cb4f602c777ee8b637f7bcfff719134beb839b94
Patrick Cool <patrickscool@gmail.com>
2021-01-03 18:08:59 -0800

    hmmm

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 const app = Vue.createApp({
     data() {
-<<<<<<< HEAD
         return {
-=======
-       return {
->>>>>>> 63946a4fb08ce2a863f5d3e5d4ce34ac02affa00
             cart: [],
             premium: true
         }
